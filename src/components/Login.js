d1f4b7f1e44862fa6d2d47cddf402357833949d8
Mina Mekhael <mina.mekhael@ymail.com>
2025-03-09 17:42:19 -0700

    Fix registration functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../firebase';
+import './Login.css';
 
 const Login = () => {
   const [email, setEmail] = useState('');
